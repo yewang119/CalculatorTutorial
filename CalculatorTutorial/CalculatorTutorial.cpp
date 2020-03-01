3e44a35d9e8fa7be2d28dc7441910973bd0259dc
yewang119 <1171184773@qq.com>
2020-03-01 20:00:45 +0800

    updated by adding more lines

--- CalculatorTutorial/CalculatorTutorial.cpp
+++ CalculatorTutorial/CalculatorTutorial.cpp
@@ -15,6 +15,10 @@ int main()
 
     cout << "Calculator Console Application" << endl << endl;
     cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b" << endl;
+    cout << "Calculator Console Application" << endl << endl;
+    cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b" << endl;
+    cout << "Calculator Console Application" << endl << endl;
+    cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b" << endl;
 
     Calculator c;
     while (true)
